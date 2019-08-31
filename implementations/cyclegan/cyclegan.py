b86e1a490b2697f78d24cab6e75d80824f747103
binbomb <38487292+binbomb@users.noreply.github.com>
2019-08-31 09:47:22 +0700

    Update cyclegan.py

--- implementations/cyclegan/cyclegan.py
+++ implementations/cyclegan/cyclegan.py
@@ -34,7 +34,7 @@ parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads 
 parser.add_argument("--img_height", type=int, default=256, help="size of image height")
 parser.add_argument("--img_width", type=int, default=256, help="size of image width")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving generator outputs")
+parser.add_argument("--sample_interval", type=int, default=5, help="interval between saving generator outputs")
 parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between saving model checkpoints")
 parser.add_argument("--n_residual_blocks", type=int, default=9, help="number of residual blocks in generator")
 parser.add_argument("--lambda_cyc", type=float, default=10.0, help="cycle loss weight")
